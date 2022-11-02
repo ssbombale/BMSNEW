fa02947a3de7ccd15f00df70715e4a0b05971cbc
ssbombale <suchita.bombale@gmail.com>
2022-11-02 10:25:15 +0530

    2nd commit

--- UserModule/src/main/java/com/user/security/jwt/JwtFliter.java
+++ UserModule/src/main/java/com/user/security/jwt/JwtFliter.java
@@ -1,12 +1,10 @@
 package com.user.security.jwt;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +13,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.user.entity.User;
 import com.user.service.UserImplementation;
-import com.user.service.UserService;
+
 @CrossOrigin(origins = "*")
 public class JwtFliter extends OncePerRequestFilter {
 	@Autowired
@@ -27,34 +23,33 @@ public class JwtFliter extends OncePerRequestFilter {
 	@Autowired
 	private UserImplementation useraccountService;
 
-
 	@Override
-	protected void doFilterInternal(HttpServletRequest  request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
 		String authorizationHeader = request.getHeader("Authorization");
-		String token = null ;
+		String token = null;
 		String userName = null;
-		if(authorizationHeader!=null && authorizationHeader.startsWith("Bearer")) {
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
 			token = authorizationHeader.substring(7);
 			userName = jwtUtils.extractUsername(token);
 		}
-	
-		if(userName!=null && SecurityContextHolder.getContext().getAuthentication() == null) {
-		//if(userName!=null) {
+
+		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			// if(userName!=null) {
 			UserDetails user = useraccountService.loadUserByUsername(userName);
-			if(jwtUtils.validateToken(token, user)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user,null);
-				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			if (jwtUtils.validateToken(token, user)) {
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+						user, null);
+				usernamePasswordAuthenticationToken
+						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-				
-				
+
 			}
 		}
-		System.out.println("------>>>>>"+response.getStatus());
+		System.out.println("------>>>>>" + response.getStatus());
 		filterChain.doFilter(request, response);
 	}
-
 
 	private String parseJwt(HttpServletRequest request) {
 		String headerAuth = request.getHeader("Authorization");
